d6d275abfeb68cfe0fb67075a1dcbffbb193a412
garvinkon <147659548+garvinkon@users.noreply.github.com>
2023-11-05 21:16:57 +0000

    We can send User Input to an API Co-authored-by: Fabian Weber <webef98@zedat.fu-berlin.de>

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TicketService } from './service/ticket.service';
 
 interface ChatMessages {
   messageText: string;
@@ -16,6 +17,8 @@ export class AppComponent {
   chatInput: string = "";
   chatMessages: ChatMessages[] = [];
 
+  constructor(private ticketService: TicketService) {}
+
   handleSend(value: string) {
     /**
      * Gets called when send button is pressed
@@ -23,6 +26,15 @@ export class AppComponent {
      */
     if (value){   //we dont want to send empty messages to backend
       this.chatMessages.push({messageText: value, isUser: true});
+      
+      this.ticketService.send(value).subscribe((response: any) => {
+        const messageText = response.messageText;
+        this.chatMessages.push({ messageText, isUser: false }); // Assuming it's a response from the server
+        console.log(response);
+      },
+      (error) => {
+        console.error('Error sending message:', error);
+      });
       console.log(value);
       this.chatInput = "";
     }  
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
