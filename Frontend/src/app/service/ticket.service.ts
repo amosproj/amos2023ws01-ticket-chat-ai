95cb6db45d1cb3bf8dacc09d11223e0401430224
garvinkon <147659548+garvinkon@users.noreply.github.com>
2023-11-05 21:16:57 +0000

    We can send User Input to an API Co-authored-by: Fabian Weber <webef98@zedat.fu-berlin.de>

--- Frontend/src/app/service/ticket.service.ts
+++ Frontend/src/app/service/ticket.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { HttpClient, HttpClientModule } from "@angular/common/http"
 
 @Injectable({providedIn: "root"})
 export class TicketService{
     constructor(private http: HttpClient){
-
     }
+    send(message: string) {
+        const url = 'https://reqres.in/api/posts'; //answer with input
+        
+        const data= { messageText: message, isUser:true };
 
+        return this.http.post(url, data,);
+    }
 }
