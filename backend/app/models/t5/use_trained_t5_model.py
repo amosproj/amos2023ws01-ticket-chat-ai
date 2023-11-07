c1a341b4ae2046a1a46e3dddf9cca69f6bdb855b
Koffi Tino Gnagniko <tino.gnagniko@gmail.com>
2023-11-07 16:22:06 +0100

    updated use_trained_t5_model.py

--- backend/app/models/t5/use_trained_t5_model.py
+++ backend/app/models/t5/use_trained_t5_model.py
@@ -5,7 +5,7 @@ model = T5ForConditionalGeneration.from_pretrained("fine_tuned_t5_model")
 tokenizer = T5Tokenizer.from_pretrained("fine_tuned_t5_model")
 
 #  input text
-input_text = "Translate this into Json: Hey, I'm David. I've encountered an issue with my company laptop's audio. There's no sound, and I've tried adjusting the volume settings, but it hasn't helped. It's affecting my ability to participate in virtual meetings. Can you please assist me in resolving this audio problem?"
+input_text = "Translate this into Json: Hey, I'm David. I've encountered an issue with my company laptop's audio. There's no sound, and I've tried adjusting the, volume settings, but it hasn't helped. It's affecting my ability to participate in virtual meetings. Can you please assist me in resolving this audio problem?"
 
 # Tokenize and convert to tensor
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
