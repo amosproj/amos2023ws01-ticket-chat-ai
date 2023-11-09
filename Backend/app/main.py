bd0f14ce8388f88571115a1b6726b938a1875f8c
Sajjad <sajjadmurtaza.nxb@gmail.com>
2023-11-09 17:08:48 +0100

    Integrate configuration and use it to define the port

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from config import AppConfig  # Import the AppConfig class from the configuration file
 from app.api.v1 import text_endpoint
 
 app = FastAPI()
 
 # Include the router from the text_endpoint module
 app.include_router(text_endpoint.router, prefix="/api/v1")
+
+if __name__ == "__main__":
+   import uvicorn
+
+   uvicorn.run(app, host="0.0.0.0", port=AppConfig.app_port)  # Use the configured port from the AppConfig class
