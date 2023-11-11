05660126dcbbec9e17cc3fc8483f9e1979110cbd
Irild <irhox100@gmail.com>
2023-11-11 21:54:01 +0100

    create imap server + part of scheduler
    Signed-off-by: Irild <irhox100@gmail.com>

--- Backend/app/email/imap_server.py
+++ Backend/app/email/imap_server.py
@@ -2,35 +2,47 @@ import imaplib
 import email
 import os
 from dotenv import load_dotenv
+import sched, time
+scheduler = sched.scheduler(time.monotonic, time.sleep)
 
-load_dotenv(dotenv_path='.env')
 
-imap_server = os.getenv('IMAP_SERVER')
-email_address = os.getenv('EMAIL_ADDRESS')
-password = os.getenv('PASSWORD')
+def receive_unseen_emails():
+    load_dotenv(dotenv_path='.env')
 
-imap = imaplib.IMAP4_SSL(imap_server)
-imap.login(email_address, password) #TODO: Change gmail settings to allow less secure apps to access email account
+    imap_server = os.getenv('IMAP_SERVER')
+    email_address = os.getenv('EMAIL_ADDRESS')
+    password = os.getenv('PASSWORD')
 
-imap.select("Inbox")
+    imap = imaplib.IMAP4_SSL(imap_server, port=993)
+    imap.login(email_address, password)
 
-_, msgNums = imap.search(None, "ALL")
+    imap.select("Inbox")
 
-for msgNum in msgNums[0].split():
-    _, data = imap.fetch(msgNum, "(RFC822)")
+    _, msgNums = imap.search(None, "UNSEEN")
 
-    message = email.message_from_bytes(data[0][1])
+    for msgNum in msgNums[0].split():
+        _, data = imap.fetch(msgNum, "(RFC822)")
 
-    print(f"Message Number: {msgNum}")
-    print(f"From: {message.get('From')}")
-    print(f"To: {message.get('To')}")
-    print(f"BCC: {message.get('BCC')}")
-    print(f"Date: {message.get('Date')}")
-    print(f"Subject: {message.get('Subject')}")
+        message = email.message_from_bytes(data[0][1])
 
-    print("Content:")
-    for part in message.walk():
-        if part.get_content_type() == "text/plain":
-            print(part.as_string())
+        print(f"Message Number: {msgNum}")
+        print(f"From: {message.get('From')}")
+        print(f"Subject: {message.get('Subject')}")
 
-imap.close()
+        print("Content:")
+        for part in message.walk():
+            if part.get_content_type() == "text/plain":
+                print(part.as_string())
+
+    imap.close()
+
+
+def scheduler_method():
+    print("Tick")
+    receive_unseen_emails()
+
+
+scheduler.enter(5, 1, scheduler_method, ())
+scheduler.enter(5, 1, scheduler_method, ())
+scheduler.run()
+
