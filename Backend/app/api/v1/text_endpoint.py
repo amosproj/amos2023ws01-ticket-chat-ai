b88c6ce4dedd52960505aa1aa17a5d7e1f8371b5
Koffi Tino Gnagniko <tino.gnagniko@gmail.com>
2023-11-12 15:04:13 +0100

    Refactored code to match formating standards

--- Backend/app/api/v1/text_endpoint.py
+++ Backend/app/api/v1/text_endpoint.py
@@ -5,22 +5,25 @@ from app.models.t5.use_trained_t5_model import TrainedT5Model
 
 router = APIRouter()
 
+
 @router.post("/text")
 async def process_text(text_input: TextInput):
     """
     Receive Text from the Frontend
-    
+
     Args:
     - text_input (TextInput): A Pydantic model defining the expected input format containing the 'text' field.
-    
+
     Returns:
     - TextResponse: A response containing the received text and a status code.
-    
+
     Raises:
     - HTTPException: If 'text' field is empty, returns a 400 Bad Request with an error message.
     """
     if not text_input.text:
-        raise HTTPException(status_code=400, detail="Text is required")  # Return a 400 Bad Request with an error message
+        raise HTTPException(
+            status_code=400, detail="Text is required"
+        )  # Return a 400 Bad Request with an error message
 
     # run model
     trained_t5_model = TrainedT5Model()
@@ -28,7 +31,7 @@ async def process_text(text_input: TextInput):
 
     # Print the received text for debugging or logging purposes
     print(f"Received Text: {received_text}")
-    
+
     # Respond with a simple message and a status code
     response_data = "Message was received"
     status_code = 200  # You can change this as needed
