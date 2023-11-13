2a17bd6bb05c70e2bea62aeb3b6910107d74b719
Irild <irhox100@gmail.com>
2023-11-13 20:07:23 +0100

    refactoring of api url, add angular material for redesign
    Co-authored-by: Garvin Konopka <konopkagarvin@gmail.com>
    Signed-off-by: Irild <irhox100@gmail.com>

--- Frontend/src/app/service/ticket.service.ts
+++ Frontend/src/app/service/ticket.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { Observable } from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class TicketService {
-  private apiUrl = 'http://127.0.0.1:8000/api/v1/text';
+  private apiUrl = environment.apiUrl + 'api/v1/text';
 
   constructor(private http: HttpClient) {}
 
