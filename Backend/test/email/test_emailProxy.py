6bc7e6adef47d5cf263d6076d84a1d75253a79c8
Fabian Weber <webef98@zedat.fu-berlin.de>
2023-11-14 11:04:01 +0000

    changed format
    Signed-off-by: Fabian Weber <webef98@zedat.fu-berlin.de>

--- Backend/test/email/test_emailProxy.py
+++ Backend/test/email/test_emailProxy.py
@@ -5,23 +5,23 @@ import pytest
 import configparser
 import imaplib
 
-load_dotenv()
 
-password = os.getenv('PASSWORD')
+load_dotenv()
+password = os.getenv("PASSWORD")
 config = configparser.ConfigParser()
-config.read('config.ini')
-imap_server = config['DEFAULT']['IMAP_SERVER']
-email_address = config['DEFAULT']['EMAIL_ADDRESS']
+config.read("config.ini")
+imap_server = config["DEFAULT"]["IMAP_SERVER"]
+email_address = config["DEFAULT"]["EMAIL_ADDRESS"]
+
 
 def test_email():
 
     with EmailProxy(imap_server, email_address, password) as proxy:
-        #check if the class has been constructed
+        # check if the class has been constructed
         assert isinstance(proxy, EmailProxy)
-        #check if we can fetch email from the mail server
+        # check if we can fetch email from the mail server
         assert proxy.spin()
 
-    #check if the connection got closed
+    # check if the connection got closed
     with pytest.raises(imaplib.IMAP4.error):
         proxy.imap.check()
-    
