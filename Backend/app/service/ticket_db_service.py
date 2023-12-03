55a6a7e22d8b432e3a928c52090b13e4aeafc97d
garvinkon <konopkagarvin@gmail.com>
2023-12-04 00:02:59 +0100

    fixed linter
    Co-authored-by: Koffi Tino Gnagniko <tino.gnagniko@gmail.com>
    Signed-off-by: garvinkon <konopkagarvin@gmail.com>

--- Backend/app/service/ticket_db_service.py
+++ Backend/app/service/ticket_db_service.py
@@ -29,7 +29,7 @@ class TicketDBService:
             )
         ticket_entity = found_tickets[0]
         return self._map_ticket(ticket_entity)
-        
+
     def update_ticket_attachments(
         self, ticket_id: str, files: list[UploadFile]
     ) -> Ticket:
