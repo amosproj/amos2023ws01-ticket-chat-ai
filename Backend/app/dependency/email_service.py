1eec1db0a29c11502354d44e28968b905e7e0068
Koffi Tino Gnagniko <tino.gnagniko@gmail.com>
2023-12-08 18:42:57 +0100

    Fix linting
    Co-authored-by: Garvin Konopka <konopkagarvin@gmail.com>
    Co-authored-by: Fabian Weber <fabian.weber98@web.de>
    Signed-off-by: Koffi Tino Gnagniko <tino.gnagniko@gmail.com>

--- Backend/app/dependency/email_service.py
+++ Backend/app/dependency/email_service.py
@@ -5,6 +5,5 @@ from app.email_router.email_service import EmailService
 from app.email_router.smtp_conn import SmtpConnection
 
 
-
 def get_email_service(smtp_connection: SmtpConnection = Depends(get_smtp_connection)):
     return EmailService(smtp_connection)
