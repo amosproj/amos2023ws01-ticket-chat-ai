e86c3ca58fb8af40fdffb8e5fd51bd76b9be0fee
Koffi Tino Gnagniko <tino.gnagniko@gmail.com>
2024-01-23 16:40:11 +0100

    Generated more test data.
    Co-authored-by: Irild Hoxhallari <irhox100@gmail.com>
    Signed-off-by: Koffi Tino Gnagniko <tino.gnagniko@gmail.com>

--- Backend/app/model/util/create_data_with_gpt.py
+++ Backend/app/model/util/create_data_with_gpt.py
@@ -2,15 +2,18 @@ from openai import OpenAI
 import json
 
 client = OpenAI(
-    api_key="APIKEY",
+    api_key="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"
 )
 
-for _ in range(5):
-    with open("../test_data/test_data_with_gpt/data_7.json", "r") as file:
+for _ in range(1):
+    with open("../test_data/test_data_with_gpt/data_9.json", "r") as file:
         tickets = json.load(file)
 
     for _ in range(100):
         try:
+
+
+
             response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
@@ -31,8 +34,8 @@ for _ in range(5):
                             "requestType": ""
                         }
                         
-                        The 'text' should contain a detailed problem description with implicit information about only one category.
-                        The 'text' should be randomly between 3 to 10 lines
+                        The 'text' should contain a detailed problem description with implicit information about the problem .
+                        The 'text' should be randomly between 5 to 10 lines
                         Furthermore, under 'ticket', there should be a support ticket matching the email with the following attributes.
                         The 'service' attribute classifies the service of the ticket with the values 'SAP ERP', 'Atlassian', 'Adobe', 'Salesforce', 'Reporting', 'Microsoft Power Platform', 'Microsoft SharePoint', 'Snowflake', 'Microsoft Office'.
                         The 'category' attribute classifies the problem dependent from service. Please extract the values from following. Before ':' states the service and after ':' are the coresponding values.  '"SAP ERP: 4HANA -> Technical Issues, 4HANA -> Billing & Payment, 4HANA -> Product Inquiries, 4HANA -> Account Management, 4HANA -> Policy Questions"
@@ -41,44 +44,32 @@ for _ in range(5):
             
                             "Atlassian: Jira -> Technical Issues, Jira -> Billing & Payment, Jira -> Product Inquiries, Jira -> Account Management, Jira -> Policy Questions"
                             "Atlassian: Sourcetree -> Technical Issues, Sourcetree -> Billing & Payment, Sourcetree -> Product Inquiries, Sourcetree -> Account Management, Sourcetree -> Policy Questions"
-                            "Atlassian: Opsgenie -> Technical Issues, Opsgenie -> Billing & Payment, Opsgenie -> Product Inquiries, Opsgenie -> Account Management, Opsgenie -> Policy Questions"
-                            "Atlassian: Trello -> Technical Issues, Trello -> Billing & Payment, Trello -> Product Inquiries, Trello -> Account Management, Trello -> Policy Questions"
             
                             "Adobe: Illustrator -> Technical Issues, Illustrator -> Billing & Payment, Illustrator -> Product Inquiries, Illustrator -> Account Management, Illustrator -> Policy Questions"
                             "Adobe: Photoshop -> Technical Issues, Photoshop -> Billing & Payment, Photoshop -> Product Inquiries, Photoshop -> Account Management, Photoshop -> Policy Questions"
-                            "Adobe: InDesign -> Technical Issues, InDesign -> Billing & Payment, InDesign -> Product Inquiries, InDesign -> Account Management, InDesign -> Policy Questions"
-                            "Adobe: Premiere -> Technical Issues, Premiere -> Billing & Payment, Premiere -> Product Inquiries, Premiere -> Account Management, Premiere -> Policy Questions"
                             
                             "Salesforce: Apex -> Technical Issues, Apex -> Billing & Payment, Apex -> Product Inquiries, Apex -> Account Management, Apex -> Policy Questions"
                             "Salesforce: Trailhead -> Technical Issues, Trailhead -> Billing & Payment, Trailhead -> Product Inquiries, Trailhead -> Account Management, Trailhead -> Policy Questions"
-                            "Salesforce: Visualforce -> Technical Issues, Visualforce -> Billing & Payment, Visualforce -> Product Inquiries, Visualforce -> Account Management, Visualforce -> Policy Questions"
-                            "Salesforce: Sales Cloud -> Technical Issues, Sales Cloud  -> Billing & Payment, Sales Cloud  -> Product Inquiries, Sales Cloud  -> Account Management, Sales Cloud  -> Policy Questions"
                             
                             "Reporting: Tableau -> Technical Issues, Tableau -> Billing & Payment, Tableau -> Product Inquiries, Tableau -> Account Management, Tableau -> Policy Questions"
                             "Reporting: Microsoft PowerBI -> Technical Issues, Microsoft PowerBI -> Billing & Payment, Microsoft PowerBI -> Product Inquiries, Microsoft PowerBI -> Account Management, Microsoft PowerBI -> Policy Questions"
-                            "Reporting: Datasource -> Technical Issues, Datasource -> Billing & Payment, Datasource -> Product Inquiries, Datasource -> Account Management, Datasource -> Policy Questions"
-                            "Reporting: DataFlow -> Technical Issues, DataFlow -> Billing & Payment, DataFlow -> Product Inquiries, DataFlow -> Account Management, DataFlow -> Policy Questions"
                             
                             "Microsoft Power Platform: Microsoft Power Apps -> Technical Issues, Microsoft Power App -> Billing & Payment, Microsoft Power App -> Product Inquiries, Microsoft Power App -> Account Management, Microsoft Power App -> Policy Questions"
                             "Microsoft Power Platform: Microsoft Power BI -> Technical Issues, Microsoft Power BI -> Billing & Payment, Microsoft Power BI -> Product Inquiries, Microsoft Power BI -> Account Management, Microsoft Power BI -> Policy Questions"
-                            "Microsoft Power Platform: Microsoft Power Pages Automate -> Technical Issues, Microsoft Power Pages Automate -> Billing & Payment, Microsoft Power Pages Automate -> Product Inquiries, Microsoft Power Pages Automate -> Account Management, Microsoft Power Pages Automate -> Policy Questions"
-                            
+                           
                             "Microsoft SharePoint: Microsoft SharePoint -> Technical Issues, Microsoft SharePoint -> Billing & Payment, Microsoft SharePoint -> Product Inquiries, Microsoft SharePoint -> Account Management, Microsoft SharePoint -> Policy Questions"
                             "Microsoft SharePoint: SharePoint -> Technical Issues, SharePoint -> Billing & Payment, SharePoint -> Product Inquiries, SharePoint -> Account Management, SharePoint -> Policy Questions"
-                            "Microsoft SharePoint: SharePoint List -> Technical Issues, SharePoint List -> Billing & Payment, SharePoint List -> Product Inquiries, SharePoint List -> Account Management, SharePoint List -> Policy Questions"
-                            "Microsoft SharePoint: SharePoint Document Library -> Technical Issues, SharePoint Document Library -> Billing & Payment, SharePoint Document Library -> Product Inquiries, SharePoint Document Library -> Account Management, SharePoint Document Library -> Policy Questions"
+                            
                             
                             "Snowflake: Snowflake -> Technical Issues, Snowflake -> Billing & Payment, Snowflake -> Product Inquiries, Snowflake -> Account Management, Snowflake -> Policy Questions"
                             "Snowflake: SnowSQL -> Technical Issues, SnowSQL -> Billing & Payment, SnowSQL -> Product Inquiries, SnowSQL -> Account Management, SnowSQL -> Policy Questions"
                             
                             "Microsoft Office: Microsoft Office -> Technical Issues, Microsoft Office -> Billing & Payment, Microsoft Office -> Product Inquiries, Microsoft Office -> Account Management, Microsoft Office -> Policy Questions"
                             "Microsoft Office: Microsoft Word -> Technical Issues, Microsoft Word -> Billing & Payment, Microsoft Word -> Product Inquiries, Microsoft Word -> Account Management, Microsoft Word -> Policy Questions" 
-                            "Microsoft Office: Microsoft Excel -> Technical Issues, Microsoft Excel -> Billing & Payment, Microsoft Excel -> Product Inquiries, Microsoft Excel -> Account Management, Microsoft Excel -> Policy Questions" 
-                            "Microsoft Office: Microsoft PowerPoint -> Technical Issues, Microsoft PowerPoint -> Billing & Payment, Microsoft PowerPoint -> Product Inquiries, Microsoft PowerPoint -> Account Management, Microsoft PowerPoint -> Policy Questions" '
                         one category values is f.e SnowSQL -> Billing & Payment.
                         
                         
-                        The 'customerPriority' attribute describes the impact of the problem on the customer and can take the values "Disruption but can work," "Disruption cannot work," "Disruption several cannot work," and "Disruption department cannot work."
+                        The 'customerPriority' attribute describes the impact of the problem on the customer and can take the values "Disruption but can work" "Disruption cannot work" "Disruption several cannot work" and "Disruption department cannot work".
                         The 'priority' attribute classifies the relevance of the ticket with the values 'Low', 'Medium', 'High' and 'Very High'
                         The 'requestType' attribute classifies the type of ticket with the values "Incident" or 'Service Request'. Incident describes a ticket if the user has a problem or similar, and Service Request describes a ticket with which the user orders a service.
                         
@@ -101,5 +92,5 @@ for _ in range(5):
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    with open("../test_data/test_data_with_gpt/data_7.json", "w") as file:
+    with open("../test_data/test_data_with_gpt/data_9.json", "w") as file:
         json.dump(tickets, file, indent=4)
