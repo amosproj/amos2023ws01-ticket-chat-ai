e227d540c481d0784435cf876376adf948a22629
Koffi Tino Gnagniko <tino.gnagniko@gmail.com>
2024-01-29 16:45:09 +0100

    Added endpoints to fetch services and categories from db via frontend.
    Co-authored-by: Marco Martin Härtl <haertl.marco@web.de>
    Signed-off-by: Koffi Tino Gnagniko <tino.gnagniko@gmail.com>

--- Backend/app/dependency/db_service.py
+++ Backend/app/dependency/db_service.py
@@ -6,14 +6,13 @@ from app.repository.user_repository import UserRepository
 from app.service.ticket_db_service import TicketDBService
 from app.service.user_db_service import UserDBService
 
-
 def get_ticket_db_service(
-    ticket_repository: TicketRepository = Depends(get_ticket_repository),
+        ticket_repository: TicketRepository = Depends(get_ticket_repository),
 ):
     return TicketDBService(ticket_repository=ticket_repository)
 
 
 def get_user_db_service(
-    user_repository: UserRepository = Depends(get_user_repository),
+        user_repository: UserRepository = Depends(get_user_repository),
 ):
     return UserDBService(user_repository=user_repository)
