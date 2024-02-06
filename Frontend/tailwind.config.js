4ad9dc4f9bd2e66189ac43e5e46522cfd26647d5
M-HRL <haertl.marco@web.de>
2024-02-06 11:03:38 +0100

    Refactored the user experience by changing the color scheme and changing view logic plus styling
    Signed-off-by: M-HRL <haertl.marco@web.de>

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'turquoise-light': '#10ddd3',
+      'turquoise-dark': '#08726f',
+      'blue': '#0c33a0',
+      'green': '#0d9a0b',
+      'gray-dark': '#313131',
+      'gray-light': '#7f7f7f',
+    },
     extend: {},
   },
   plugins: [],
